c716f860741f39c6c556a37de5d74b1df009fea4
ArtemLunin <lunin.mail@gmail.com>
2021-07-21 21:04:06 +0300

    Day 1. Part 1

---
+++ js/main.js
@@ -0,0 +1,68 @@
+/*new Swiper('.swiper-container', {
+	loop: true,
+	navigation: {
+		nextEl: '.arrow',
+	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20
+		},
+		541: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	}
+});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+	menuButton.classList.toggle('menu-button-active');
+	menu.classList.toggle('header-active');
+})
+*/
+
+const getElement = (tagName, classNames) => {
+	const element = document.createElement(tagName);
+
+	if(classNames) {
+		element.classList.add(...classNames);
+	}
+	return element;
+};
+
+const createHeader = (param) => {
+	const header = getElement('header');
+	const container = getElement('div', ['container']);
+	const wrapper = getElement('div', ['header']);
+
+	if(param.header.logo) {
+		const logo = getElement('img', ['logo']);
+		logo.src = param.header.logo;
+		logo.alt = param.title;
+		wrapper.append(logo);
+	}
+	
+	header.append(container);
+	container.append(wrapper);
+	return header;
+};
+
+const movieConstructor = (selector, options) => {
+	const app = document.querySelector(selector);
+	app.classList.add('body-app');
+
+	if(options.header) {
+		app.append(createHeader(options));
+	}
+};
+
+movieConstructor('.app', {
+	title: 'Ведьмак',
+	header: {
+		logo: 'witcher/logo.png',
+	}
+});
+
+// 55 min
